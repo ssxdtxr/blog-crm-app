f6498e886d025030952108c3b19fea42cb831ee9
Egor <120664925+ssxdtxr@users.noreply.github.com>
2023-06-30 14:42:33 +0300

    create login and sign-in

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from "react-router-dom"
+import {Login} from "./pages/Login/Login";
+import {SignIn} from "./pages/SignIn/SignIn";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Login/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/sign-in" element={<SignIn/>}/>
+        </Routes>
+    );
 }
 
 export default App;
